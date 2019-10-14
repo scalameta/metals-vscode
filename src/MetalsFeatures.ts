6088e91f92db850a7d0b11b352afcbd0a331c21a
Olafur Pall Geirsson <lgeirsson@twitter.com>
2019-10-14 10:24:16 +0100

    Revert "Revert "Add debugging provider to client capabilities. (#148)""
    This reverts commit 6606b100ed7ca920b976b723beb011209d0c4c97.

--- src/MetalsFeatures.ts
+++ src/MetalsFeatures.ts
@@ -13,6 +13,7 @@ export class MetalsFeatures implements StaticFeature {
       params.capabilities.experimental = {};
     }
     params.capabilities.experimental.treeViewProvider = true;
+    params.capabilities.experimental.debuggingProvider = true;
   }
   fillClientCapabilities(): void {}
   initialize(capabilities: ServerCapabilities): void {
