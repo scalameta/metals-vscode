587e1813dec664c2c816e46da67a5bdc6d70dd5c
Gabriele Petronella <gabriele@buildo.io>
2020-01-18 18:19:03 +0100

    Add getJavaOptions

---
+++ packages/metals-languageclient/src/getJavaOptions.ts
@@ -0,0 +1,51 @@
+import * as fs from "fs";
+import * as path from "path";
+import { parse } from "shell-quote";
+
+export function getJavaOptions(workspaceRoot: string): string[] {
+  const allOptions = [...fromEnv(), ...fromJvmoptsFile(workspaceRoot)];
+  return allOptions.filter(isValidOption);
+}
+
+function isValidOption(option: string): boolean {
+  return (
+    option.startsWith("-") &&
+    // We care most about enabling options like HTTP proxy settings.
+    // We don't include memory options because Metals does not have the same
+    // memory requirements as for example the sbt build.
+    !option.startsWith("-Xms") &&
+    !option.startsWith("-Xmx") &&
+    !option.startsWith("-Xss")
+  );
+}
+
+function fromJvmoptsFile(workspaceRoot: string): string[] {
+  const jvmoptsPath = path.join(workspaceRoot, ".jvmopts");
+  if (fs.existsSync(jvmoptsPath)) {
+    console.debug("Using JVM options set in " + jvmoptsPath);
+    const raw = fs.readFileSync(jvmoptsPath, "utf8");
+    return raw.match(/[^\r\n]+/g) || [];
+  }
+  return [];
+}
+
+function fromEnv(): string[] {
+  function parseEnvVariable(variable: string | undefined): string[] {
+    if (variable) {
+      console.debug("Using JAVA options set in JAVA_OPTS");
+      return parse(variable).filter((entry): entry is string => {
+        if (typeof entry === "string") {
+          return true;
+        } else {
+          console.debug(`Ignoring unexpected JAVA_OPTS token: ${entry}`);
+          return false;
+        }
+      });
+    } else {
+      return [];
+    }
+  }
+  const javaOpts = parseEnvVariable(process.env.JAVA_OPTS);
+  const javaFlags = parseEnvVariable(process.env.JAVA_FLAGS);
+  return javaOpts.concat(javaFlags);
+}
