df68dfbbe05e28a53caa4875d7a8599d350866f9
Gabriele Petronella <gabriele@buildo.io>
2020-02-01 09:37:09 +0100

    Add utilities to simplify fetching and launching Metals

---
+++ packages/metals-languageclient/src/getServerOptions.ts
@@ -0,0 +1,41 @@
+import { ServerOptions } from "./interfaces/ServerOptions";
+import { JavaConfig } from "./getJavaConfig";
+
+interface GetServerOptions {
+  metalsClasspath: string;
+  serverProperties: string[];
+  clientName: string;
+  doctorFormat: "html" | "json";
+  javaConfig: JavaConfig;
+}
+
+export function getServerOptions({
+  metalsClasspath,
+  serverProperties,
+  clientName,
+  doctorFormat,
+  javaConfig: { javaOptions, javaPath, extraEnv }
+}: GetServerOptions): ServerOptions {
+  const baseProperties = [
+    `-Dmetals.client=${clientName}`,
+    `-Dmetals.doctor-format=${doctorFormat}`,
+    "-Xss4m",
+    "-Xms100m"
+  ];
+  const mainArgs = ["-classpath", metalsClasspath, "scala.meta.metals.Main"];
+
+  // let user properties override base properties
+  const launchArgs = [
+    ...baseProperties,
+    ...javaOptions,
+    ...serverProperties,
+    ...mainArgs
+  ];
+
+  const env = { ...process.env, ...extraEnv };
+
+  return {
+    run: { command: javaPath, args: launchArgs, options: { env } },
+    debug: { command: javaPath, args: launchArgs, options: { env } }
+  };
+}
--- packages/metals-languageclient/src/index.ts
+++ packages/metals-languageclient/src/index.ts
@@ -1,9 +1,11 @@
 export * from "./getJavaHome";
-export * from "./getJavaOptions";
 export * from "./downloadProgress";
 export * from "./isSupportedLanguage";
 export * from "./isDottyIdeEnabled";
 export * from "./detectLaunchConfigurationChanges";
 export * from "./checkServerVersion";
+export * from "./fetchMetals";
+export * from "./getServerOptions";
+export * from "./getJavaConfig";
 
 export * from "./commands/restartServer";
