b35fc215c16376f12fec66b6ae2ae1a95b3e2b3a
Aleksei Alefirov <aalefirov@virtuslab.com>
2020-04-06 20:29:28 +0200

    Added initial debug configuration setup

--- src/scalaDebugger.ts
+++ src/scalaDebugger.ts
@@ -9,6 +9,7 @@ import {
 
 export const startAdapterCommand = "debug-adapter-start";
 const configurationType = "scala";
+const launchRequestType = "launch";
 
 export function initialize(outputChannel: vscode.OutputChannel): Disposable[] {
   outputChannel.appendLine("Initializing Scala Debugger");
@@ -49,14 +50,137 @@ export async function start(
 
 class ScalaConfigProvider implements vscode.DebugConfigurationProvider {
   provideDebugConfigurations(): ProviderResult<DebugConfiguration[]> {
-    return [];
+    const mainClassPick = "Main Class";
+    const testClassPick = "Test Suite";
+
+    return vscode.window
+      .showQuickPick([mainClassPick, testClassPick], {
+        placeHolder: "Pick kind of class to debug"
+      })
+      .then(result => {
+        if (result === mainClassPick) {
+          return this.provideDebugMainClassConfiguration().then(config => {
+            if (config === undefined) return [];
+            return [config];
+          });
+        } else if (result === testClassPick) {
+          return this.provideDebugTestClassConfiguration().then(config => {
+            if (config === undefined) return [];
+            return [config];
+          });
+        }
+        return [];
+      });
   }
 
   resolveDebugConfiguration(
     _folder: WorkspaceFolder | undefined,
     debugConfiguration: DebugConfiguration
   ): ProviderResult<DebugConfiguration> {
+    if (debugConfiguration.type === undefined) return null;
     return debugConfiguration;
+  }
+
+  private provideDebugMainClassConfiguration(): Thenable<
+    DebugConfiguration | undefined
+  > {
+    return this.askForBuildTarget().then(buildTarget => {
+      if (buildTarget === undefined) return undefined;
+      return this.askForClassName().then(className => {
+        if (className === undefined) return undefined;
+        return this.askForArguments().then(args => {
+          if (args === undefined) return undefined;
+          return this.askForConfigurationName(className).then(name => {
+            if (name === undefined) return undefined;
+            const result: DebugConfiguration = {
+              type: configurationType,
+              name: name,
+              request: launchRequestType,
+              mainClass: className,
+              buildTarget: buildTarget,
+              args: args
+            };
+            return result;
+          });
+        });
+      });
+    });
+  }
+
+  private provideDebugTestClassConfiguration(): Thenable<
+    DebugConfiguration | undefined
+  > {
+    return this.askForBuildTarget().then(buildTarget => {
+      if (buildTarget === undefined) return undefined;
+      return this.askForClassName().then(className => {
+        if (className === undefined) return undefined;
+        return this.askForConfigurationName(className).then(name => {
+          if (name === undefined) return undefined;
+          const result: DebugConfiguration = {
+            type: configurationType,
+            name: name,
+            request: launchRequestType,
+            testClass: className,
+            buildTarget: buildTarget
+          };
+          return result;
+        });
+      });
+    });
+  }
+
+  private askForBuildTarget(): Thenable<string | null | undefined> {
+    return vscode.window
+      .showInputBox({
+        prompt: "Enter name of the build target",
+        placeHolder: "Optional, you can leave it empty"
+      })
+      .then(buildTarget => {
+        if (buildTarget === undefined) {
+          return undefined;
+        } else if (buildTarget === "") {
+          return null;
+        } else {
+          return buildTarget;
+        }
+      });
+  }
+
+  private askForClassName(): Thenable<string | undefined> {
+    return vscode.window.showInputBox({
+      prompt: "Enter name of the class to debug",
+      placeHolder: "<package>.<Class>"
+    });
+  }
+
+  private askForConfigurationName(
+    className: string
+  ): Thenable<string | undefined> {
+    return vscode.window.showInputBox({
+      prompt: "Enter name of the configuration",
+      value: `Debug ${className}`
+    });
+  }
+
+  private askForArguments(): Thenable<string[] | undefined> {
+    return vscode.window
+      .showInputBox({ prompt: "Enter argument or leave it empty" })
+      .then(argument => {
+        if (argument === undefined) {
+          return undefined;
+        } else if (argument === "") {
+          return [];
+        } else {
+          return this.askForArguments().then(rest => {
+            if (rest === undefined) {
+              return [argument];
+            } else {
+              rest.unshift(argument);
+              return rest;
+            }
+          });
+        }
+      });
   }
 }
 
