f145c0c46da7f8d571ac243875d4ba37a8114d8b
Vadim Chelyshov <qtankle@gmail.com>
2022-02-16 14:40:08 +0300

    suggestLatestUpgrade - enable by default for pre-release

--- update-version.js
+++ update-version.js
@@ -3,6 +3,12 @@ const fs = require("fs");
 const path = "./package.json";
 
 const package = fs.readFileSync(path, { encoding: "utf8", flag: "r" });
-const updated = package.replaceAll("1.59.0", "1.63.0");
+const json = JSON.parse(package);
+json.contributes.configuration.properties[
+  "metals.suggestLatestUpgrade"
+].default = true;
+json.engines.vscode = "^1.63.0";
+json.devDependencies["@types/vscode"] = "1.63.0";
 
+const updated = JSON.stringify(json, null, 2);
 fs.writeFileSync(path, updated);
