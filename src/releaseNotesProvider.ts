fb01b8c5c953b8123ecdc6fd8f48a4c3d6a80586
Kamil Podsiadlo <kamilpodsiadlo44@gmail.com>
2022-06-12 11:57:23 +0200

    fix: move remarkable to normal dependencies fix: show releas enotes on wsl

--- src/releaseNotesProvider.ts
+++ src/releaseNotesProvider.ts
@@ -88,8 +88,7 @@ async function showReleaseNotes(
    * TODO: what about wsl?
    */
   function isRemote(): Either<string, void> {
-    // const isWsl = env.remoteName === "wsl";
-    return env.remoteName == null
+    return env.remoteName == null || env.remoteName !== "wsl"
       ? makeRight(undefined)
       : makeLeft(`is a remote environment ${env.remoteName}`);
   }
