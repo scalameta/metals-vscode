3ebd387c3b228c13e760540072fb3db15eb20bfc
tgodzik <tgodzik@virtuslab.com>
2024-04-18 15:32:49 +0200

    bugfix: Remove locate-java-home
    It seems it uses promises underneath and if anything throws it just hangs, so Metals will never start. I amanged to reproduce it locally, though hopefully doesn't reproduce in a lot of machines

--- packages/metals-languageclient/src/getJavaHome.ts
+++ packages/metals-languageclient/src/getJavaHome.ts
@@ -1,15 +1,3 @@
-import { TaskEither, chain } from "fp-ts/lib/TaskEither";
-import * as TE from "fp-ts/lib/TaskEither";
-import * as E from "fp-ts/lib/Either";
-import { pipe } from "fp-ts/lib/function";
-import _locateJavaHome from "@viperproject/locate-java-home";
-import * as semver from "semver";
-import {
-  ILocateJavaHomeOptions,
-  IJavaHomeInfo,
-} from "@viperproject/locate-java-home/js/es5/lib/interfaces";
-import { toPromise } from "./util";
-import fs from "fs";
 import path from "path";
 import { spawn } from "promisify-child-process";
 import { OutputChannel } from "./interfaces/OutputChannel";
@@ -31,7 +19,7 @@ export async function getJavaHome(
   outputChannel: OutputChannel
 ): Promise<string | undefined> {
   const fromEnvValue = await fromEnv(javaVersion, outputChannel);
-  return fromEnvValue ? fromEnvValue : await locate(javaVersion);
+  return fromEnvValue;
 }
 
 const versionRegex = /\"\d\d/;
@@ -79,74 +67,4 @@ export async function fromEnv(
   }
 
   return undefined;
-}
-
-function locate(javaVersion: JavaVersion): Promise<undefined | string> {
-  return toPromise(
-    pipe(
-      locateJavaHome({ version: `>=${javaVersion}` }),
-      chain((javaHomes) => {
-        if (!javaHomes || javaHomes.length === 0) {
-          return TE.right(undefined);
-        } else {
-          const jdkHomes = javaHomes.filter((j) => j.isJDK);
-          const fromBinPath = matchesBinFromPath(jdkHomes);
-          const jdkHome = fromBinPath ? fromBinPath : latestJdk(jdkHomes);
-          if (jdkHome) {
-            return TE.right(jdkHome.path);
-          } else {
-            return TE.right(javaHomes[0].path);
-          }
-        }
-      })
-    )
-  );
-}
-
-function matchesBinFromPath(
-  jdkHomes: IJavaHomeInfo[]
-): IJavaHomeInfo | undefined {
-  const value = process.env["PATH"];
-  if (value && jdkHomes.length > 0) {
-    const result = value
-      .split(path.delimiter)
-      .map((p) => path.join(p, "java"))
-      .filter((p) => fs.existsSync(p));
-
-    if (result.length > 0) {
-      const realpath = fs.realpathSync(result[0]);
-      const matched = jdkHomes.find((home) => {
-        const javaBin = path.join(home.path, "bin", "java");
-        return javaBin == realpath;
-      });
-      return matched;
-    } else {
-      return undefined;
-    }
-  } else {
-    return undefined;
-  }
-}
-
-function latestJdk(jdkHomes: IJavaHomeInfo[]): IJavaHomeInfo | undefined {
-  if (jdkHomes.length > 0) {
-    return jdkHomes.sort((a, b) => {
-      const byVersion = -semver.compare(a.version, b.version);
-      if (byVersion === 0) return b.security - a.security;
-      else return byVersion;
-    })[0];
-  } else {
-    return undefined;
-  }
-}
-
-function locateJavaHome(
-  opts: ILocateJavaHomeOptions
-): TaskEither<Error, IJavaHomeInfo[] | undefined> {
-  return () =>
-    new Promise((resolve) =>
-      _locateJavaHome(opts, (err, res) =>
-        err != null ? resolve(E.left(err)) : resolve(E.right(res))
-      )
-    );
 }
